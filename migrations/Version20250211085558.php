dc170d7065d8d1fd570d2f5bfe7311c49d840746
Anthony RUIZ <anthony.ruiz@lulilo.fr>
2025-02-20 10:54:44 +0100

    correction erreur

--- migrations/Version20250211085558.php
+++ migrations/Version20250211085558.php
@@ -20,27 +20,17 @@ final class Version20250211085558 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE article_id_seq CASCADE');
         $this->addSql('CREATE TABLE product (id SERIAL NOT NULL, category_id INT NOT NULL, brand_id INT NOT NULL, title VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, reference_supplier VARCHAR(255) NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_D34A04AD12469DE2 ON product (category_id)');
         $this->addSql('CREATE INDEX IDX_D34A04AD44F5D008 ON product (brand_id)');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD44F5D008 FOREIGN KEY (brand_id) REFERENCES brand (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE article DROP CONSTRAINT fk_23a0e6612469de2');
-        $this->addSql('ALTER TABLE article DROP CONSTRAINT fk_23a0e6644f5d008');
-        $this->addSql('DROP TABLE article');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE article_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE article (id SERIAL NOT NULL, category_id INT NOT NULL, brand_id INT NOT NULL, title VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, reference_supplier VARCHAR(255) NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX idx_23a0e6644f5d008 ON article (brand_id)');
-        $this->addSql('CREATE INDEX idx_23a0e6612469de2 ON article (category_id)');
-        $this->addSql('ALTER TABLE article ADD CONSTRAINT fk_23a0e6612469de2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE article ADD CONSTRAINT fk_23a0e6644f5d008 FOREIGN KEY (brand_id) REFERENCES brand (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD12469DE2');
         $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD44F5D008');
         $this->addSql('DROP TABLE product');
